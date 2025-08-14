86e6d8694559e07cd6414e853a91efcb7902ee8e
davejinyoung <davejinyoung@gmail.com>
2025-08-13 21:44:57 -0700

    Showing real running apps with UI

--- desktop-app-switcher/ContentView.swift
+++ desktop-app-switcher/ContentView.swift
@@ -6,33 +6,31 @@
 //
 
 import SwiftUI
+import AppKit
 
 struct AppInfo: Identifiable {
-    let id = UUID()
+    let id: String
     let name: String
-    let iconSystemName: String
+    let icon: NSImage
 }
 
-let sampleApps = [
-    AppInfo(name: "Finder", iconSystemName: "folder.fill"),
-    AppInfo(name: "Safari", iconSystemName: "safari.fill")
-]
-
 struct ContentView: View {
-    @State private var selectedAppID: UUID? = sampleApps.first?.id
+    @State private var runningApps: [AppInfo] = []
+    @State private var selectedAppID: String?
     
     var body: some View {
-        HStack(spacing: 20){
-            ForEach(sampleApps) { app in
+        HStack(){
+            ForEach(runningApps) { app in
                 VStack(alignment: .center) {
-                    Image(systemName: app.iconSystemName)
+                    Image(nsImage: app.icon)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 64, height: 64)
-                        .padding(12)
+                        .frame(width: 90, height: 90)
+                        .padding(8)
                         .background(selectedAppID == app.id ? Color.black.opacity(0.25) : Color.clear)
                         .cornerRadius(8)
-                    Text(selectedAppID == app.id ? app.name : "")
+                    Text(app.name)
+                        .lineLimit(1)
                 }
                 .onTapGesture {
                     selectedAppID = app.id
@@ -42,6 +40,22 @@ struct ContentView: View {
         .padding(25)
         .cornerRadius(8)
         .frame(width: 700, height: 160)
+        .onAppear(perform: fetchRunningApps)
+    }
+    
+    private func fetchRunningApps() {
+        let runningApps = NSWorkspace.shared.runningApplications
+            .filter { $0.activationPolicy == .regular }
+            .compactMap{ app -> AppInfo? in
+                guard let name = app.localizedName,
+                      let icon = app.icon,
+                      let id = app.bundleIdentifier else {
+                    return nil
+                }
+                return AppInfo(id: id, name: name, icon: icon)
+            }
+        self.runningApps = runningApps
+        self.selectedAppID = runningApps.first?.id
     }
 }
 
