2d4d4ef0309ea18eeac2e59aca5364fc751d8ea2
davejinyoung <davejinyoung@gmail.com>
2025-12-14 22:41:46 -0800

    Proportionally sizing elements relative to panel size

--- desktop-app-switcher/Views/ContentView.swift
+++ desktop-app-switcher/Views/ContentView.swift
@@ -1,29 +1,15 @@
 import AppKit
 import SwiftUI
 
-struct VisualEffectView: NSViewRepresentable {
-    let material: NSVisualEffectView.Material
-    let blendingMode: NSVisualEffectView.BlendingMode
-    
-    func makeNSView(context: Context) -> NSVisualEffectView {
-        let view = NSVisualEffectView()
-        view.blendingMode = blendingMode
-        view.material = material
-        view.state = .active
-        return view
-    }
-    
-    func updateNSView(_ nsView: NSVisualEffectView, context: Context) {
-        nsView.material = material
-    }
-}
-
-
 struct ContentView: View {
     @EnvironmentObject var appState: AppState
     @State private var selectedAppId: String?
 
     var body: some View {
+        let appBackgroundSize = appState.appIconSize + (0.03125 * (200-appState.appIconSize))
+        let appBackgroundCornerRadius = 13 + (((appState.appIconSize - 40) / 160) * 37) // max=50, min=13
+        let panelCornerRadius = 20 + (((appState.appIconSize - 40) / 160) * 40) // max=60, min=20
+        
         HStack {
             ForEach(appState.runningApps) { app in
                 VStack {
@@ -35,8 +21,10 @@ struct ContentView: View {
                         .background(
                             Group {
                                 if selectedAppId == app.id {
-                                    RoundedRectangle(cornerRadius: appState.appIconSize * 0.2, style: .continuous)
+                                    RoundedRectangle(cornerRadius: appBackgroundCornerRadius, style: .continuous)
                                         .fill(Color.gray.opacity(0.7))
+                                        .frame(width: appBackgroundSize, height: appBackgroundSize)
+                                        
                                 }
                             }
                         )
@@ -44,10 +32,10 @@ struct ContentView: View {
                         .overlay(alignment: .bottom) {
                             Text(app.name)
                                 .frame(width: appState.appIconSize * 0.8)
-                                .font(.system(size: 13, weight: .light, design: .default))
+                                .font(.system(size: 13, weight: .regular, design: .default))
                                 .lineLimit(1)
                                 .foregroundColor(selectedAppId == app.id ? Color.white : Color.clear)
-                                .offset(y: 18)
+                                .offset(y: 19)
                         }
                 }
                 .onHover { hover in // The onHover now wraps the whole item
@@ -58,7 +46,7 @@ struct ContentView: View {
             }
         }
         .padding(25)
-        .glassEffect(.clear, in: .rect(cornerRadius: 50))
+        .glassEffect(.clear, in: .rect(cornerRadius: panelCornerRadius))
         .frame(width: appState.screenWidth, height: appState.screenHeight)
         .onChange(of: appState.selectedAppId) {
             selectedAppId = appState.selectedAppId
